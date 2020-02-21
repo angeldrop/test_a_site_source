1353502f45dd48565f39d26e9cd9f8883da993d1
DanTei <fffdan111@163.com>
2020-02-21 09:56:25 +0800

    添加了unittest功能测试

--- TestTDD.py
+++ TestTDD.py
@@ -1,5 +1,23 @@
 # http://175.24.111.140:8080
 from selenium import webdriver
-browser=webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser=webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #迪达拉听说了一个很酷的在线待办事项应用
+        #他去看了应用首页
+        self.browser.get('http://localhost:8000')
+        
+        #他注意到标题处有“To-Do”这个词
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('Finish the test!')
+        
+        #应用邀请他输入一个代办事项
+        
+if __name__=='__main__':
+    unittest.main()    #warnings='ignore'
