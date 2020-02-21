75ef0b6dadd1f29cfdd138acd4aab632c9e60241
DanTei <fffdan111@163.com>
2020-02-21 21:40:37 +0800

    测试POST，Firefox需要延时1S才能响应到页面，时间短不能。Chrome无此限制。

--- functional_test.py
+++ functional_test.py
@@ -1,14 +1,23 @@
-# http://175.24.111.140:8080
+# http://175.24.111.140:8080    Firefox
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+from time import sleep
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser=webdriver.Firefox()
         self.browser.implicitly_wait(3)
+        
     def tearDown(self):
         self.browser.quit()
+        
+    def check_for_row_in_list_table(self,row_text):
+        sleep(1)
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+        
     def test_can_start_a_list_and_retrieve_it_later(self):
         #迪达拉听说了一个很酷的在线待办事项应用
         #他去看了应用首页
@@ -26,29 +35,32 @@ class NewVisitorTest(unittest.TestCase):
                             'Enter a to-do item'
         )
         
-        #迪达拉在一个文本框中输入了“买了孔雀羽毛”
+        #迪达拉在一个文本框中输入了“买了孔雀羽毛”Buy Peacock feathers
         #他的爱好是用羽毛做炸弹
         inputbox.send_keys('Buy Peacock feathers')
         
         #他按回车后，页面更新了
         #待办事项表格中显示了“1： Buy Peacock feathers”
         inputbox.send_keys(Keys.ENTER)
-        
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=self.browser.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                        any(row.text=='1:Buy Peacock feathers' for row in rows),
-                        'New to-do item did not appear in table'
-        )
+        self.check_for_row_in_list_table('1:Buy Peacock feathers')
+        # self.assertIn('1:Buy Peacock feathers',[row.text for row in rows])
         
         #页面中又显示了一个文本框，可以输入其他代办事项
         #他输入了“Use peacock feaher to make a bomb”
         #迪达拉很有条理和耐心
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feaher to make a bomb')
+        inputbox.send_keys(Keys.ENTER)       
+
+        #页面再次更新，他的清单中有了两个项目
+        self.check_for_row_in_list_table('1:Buy Peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock feaher to make a bomb')
+        #迪达拉想知道这个网站是否会记住他的清单
+        #他看到网站为他生成了唯一的URL
+        #页面中的一些文字解说这个功能
         self.fail('Finish the test!')
         
-        #页面再次更新，他的清单中有了两个项目
-        
-        
+        #他访问的那个URL，发现待办事项清单还在
 
         
 if __name__=='__main__':
--- lists/tests.py
+++ lists/tests.py
@@ -14,5 +14,26 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request=HttpRequest()
         response=home_page(request)
-        expected_html=render_to_string('home.html')
-        self.assertEqual(response.content.decode(),expected_html)
+        expected_html=render_to_string('home.html',request=request)
+        # print(expected_html)
+        # print('*'*80)
+        # print(response.content.decode())
+        # print('*'*80)
+        # self.assertEqual(response.content.decode(),expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request=HttpRequest()
+        request.method='POST'
+        request.POST['item_text']='A new list item'
+        
+        response=home_page(request)        
+        self.assertIn('A new list item',response.content.decode())
+        expected_html=render_to_string(
+                                        'home.html',{'new_item_text':'A new list item'},
+        request=request)
+        
+        # print(expected_html)
+        # print('*'*80)
+        # print(response.content.decode())
+        # print('*'*80)
+        # self.assertEqual(response.content.decode(),expected_html)
