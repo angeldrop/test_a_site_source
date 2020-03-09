e155cc060d91fc947e2ab5c26d7f34d2135be4bc
DanTei <fffdan111@163.com>
2020-03-09 20:47:10 +0800

    数据库层面防止重复值发生

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -42,7 +42,21 @@ class ItemValidationTest(FunctionalTest):
         self.check_for_row_in_list_table('2:Make tea')
 
 
-
+    def test_cannot_add_duplicate_items(self):
+        #didala访问首页，新建一个清单
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1:Buy wellies')
+        
+        #他不小心输入了一个重复待办事项
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        
+        #他看到了一条有帮助的错误消息
+        self.check_for_row_in_list_table('1:Buy wellies')
+        error=self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text,"You've already got this in your list")
 
 
 
