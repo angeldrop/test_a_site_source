1d9e4d9e143a2b8822dbc3a8bde995e7bf0aa46d
DanTei <fffdan111@163.com>
2020-03-10 19:13:26 +0800

    修改lists.js使其在页面元素加载完后执行

--- lists/static/tests/tests.js
+++ lists/static/tests/tests.js
@@ -1,10 +1,10 @@
 QUnit.test( "error should be hidden on keypress", function( assert ) {
-  initialize();
+  window.Superlists.initialize();
   $('input[name="text"]').trigger('keypress');
   assert.equal($('.has-error').is(':visible'),false)
 });
 
 QUnit.test( "error aren't hidden if there is no keypress", function( assert ) {
-  initialize();
+  window.Superlists.initialize();
   assert.equal($('.has-error').is(':visible'),true)
 });
