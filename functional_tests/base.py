7555f8fd3a714fe4b5be62e105b609a812559d05
angeldrop <fffdan111@163.com>
2020-05-21 11:17:27 +0800

    使用了装饰器

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -28,7 +28,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         for arg in sys.argv:
             if 'liveserver' in arg:
                 cls.server_url='http://'+arg.split('=')[1]
-                return
+                
         super().setUpClass()
         cls.server_url=cls.live_server_url
         
